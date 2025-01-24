18d2ee546b2c87fe11412987419f8197c9b4baa5
AlexElaryan <155388139+AlexElaryan@users.noreply.github.com>
2025-01-25 00:02:39 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -15,22 +15,21 @@ let line = document.querySelectorAll('.line svg');
 
 marker.forEach((el, ind) => {
     el.onmouseenter = () => {
-        el.children[2].classList.add('marker-animation_active');
+        el.children[1].classList.add('marker-animation_active');
         if (el !== marker[marker.length - 1]) {
             line[ind].classList.add('line_active_right');
             if (objKeys[ind]) {
-                info.classList.add('info_block_active');       
+                info.classList.add('info_block_active');
                 infoText.textContent = obj[objKeys[ind]][0];
-                
-                if (obj[objKeys[ind]][1]){
+
+                if (obj[objKeys[ind]][1]) {
                     gymPicture.src = obj[objKeys[ind]][1];
-                    console.log(obj[objKeys[ind]][1]);
                 }
             }
         }
     }
     el.onmouseleave = () => {
-        el.children[2].classList.remove('marker-animation_active');
+        el.children[1].classList.remove('marker-animation_active');
         info.classList.remove('info_block_active');
         gymPicture.src = 'img/gym.png';
         line[ind].classList.remove('line_active_right');
